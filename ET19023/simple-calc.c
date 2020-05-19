dbac33e975c1e2b56a687fbadd54a7cdfcb4b2db
DHBW-TEL19GR2-et19023 <65611348+DHBW-TEL19GR2-et19023@users.noreply.github.com>
2020-05-19 18:47:13 +0200

    Create simple-calc.c

---
+++ ET19023/simple-calc.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+enum Operations {
+            PLUS  = 0,
+            MINUS = 1,
+            MULT  = 2,
+            DIV   = 3
+};
+
+// Performs the given Operation on a and b
+// Returns the result c = a <op> b
+int perform_operation(int a, int b, int op) {
+    int c = 0;
+    if (op == PLUS) {
+        c = perform_sum(a, b);
+    }
+    return c;
+}
+
+// Performs the "plus" operation on a and b
+// Returns the result c = a + b
+int perform_sum (int a, int b) {
+
+    int c = 0;
+    c = a + b;
+
+    return c;
+}
+
+// Prints results of a <op> b = c
+void ausgabe (int a, int b, int op, int c) {
+    if (op == PLUS) {
+        printf("%d + %d = %d", a, b, c);
+    }
+}
+
+int main() {
+
+    int a = 1;
+    int b = 1;
+    int c = 0;
+    int op = 0;
+
+    c = perform_operation(a, b, op);
+    ausgabe(a, b, op, c);
+
+    // int c = a PLUS b;
+}
