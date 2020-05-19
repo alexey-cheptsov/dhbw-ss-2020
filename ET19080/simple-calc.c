cdd268be9b4b1b7aece9d0957fae615b0f299081
DHBW-TEL19GR2-et19080 <65611241+DHBW-TEL19GR2-et19080@users.noreply.github.com>
2020-05-19 18:23:36 +0200

    Update simple-calc.c

--- ET19080/simple-calc.c
+++ ET19080/simple-calc.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
 
-enum Operations {
-    PLUS = 0,
-    MINUS = 1,
-    MULT = 2,
-    DIV = 3
+enum Operations{
+            PLUS =0,
+            MINUS=1,
+            MULT=2,
+            DIV=3,
+
 };
 
+//Verantwortlichkeiten festlegen
+
+//Kollege 1
+//int perform_operation(int a, int b, int op);
+
+//Kollege 2
+//int perform_sum(int a, int b);
+
+//Kollege 3
+//void ausgabe( int a, int b, int op, int c);
+
+
+
 //Performs the given Operation on a and b
-//returns the result c = a <op> b
+//Returns the result c = a <op> b
 int perform_operation(int a, int b, int op){
-    int c=0;
+    int c = 0;
 
-    if(op == PLUS)
-        c = perform_sum(a,b);
-
-    return c;
-}
-
-//Performs the plus-operation on a and b
-//returns the result c = a + b
-int perform_sum(int a, int b) {
-    int c=0;
-
-    c=a+b;
-
-    return c;
-}
-
-//Prints result of a <op> b = c
-void ausgabe(int a, int b, int op, int c) {
-    printf("results of operation: ");
-    printf("%d", a);
     if (op == PLUS)
-        printf("%c", '+');
-    printf(" %d ", b);
-    printf(" = %d\n", c);
+        c = perform_sum(a, b);
+
+    return c;
 }
 
-int main() {
-    int a = 1;
-    int b = 1;
+//Performs the "plus" Operation
+//Returns the result c = a + b
+int perform_sum(int a, int b){
+    int c = 0;
 
-    //int c = a PLUS b
-    int c=0;
-    c = perform_operation(a,b, PLUS);
-    ausgabe(a,b,PLUS,c);
+    c = a+b;
+
+    return c;
+
+}
+
+//prints the result of a <op> b = c
+void ausgabe( int a, int b, int op, int c){
+
+    printf("Result of operation: ");
+    printf("%d ", a);
+    if(op==PLUS)
+        printf("%c ", '+');
+    printf("%d ", b);
+    printf(" = %d\n", c);
+
 }
