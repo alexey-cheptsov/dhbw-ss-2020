ebc68b410f7ea9da192b55d374b669b4a4c9a303
DHBW-TEL19GR2-et19036 <et19036@lehre.dhbw-stuttgart.de>
2020-06-09 18:45:39 +0200

    update1

--- Termin_4-Million/Gruppe2/d.c
+++ Termin_4-Million/Gruppe2/d.c
@@ -2,20 +2,49 @@ typedef struct Question;
 typedef struct Player;
 typedef struct State;
 
-State* stateMenu;
-State* statePlaying;
-State* stateJoker;
-State* stateWon;
-State* stateLost;
+typedef struct {
+	char* question;
+	char* answers[4];
+	int nr_correct;
+} Question;
 
-State* currentState = stateMenu;
+typedef struct {
+	int credits;
+	int level;
+	_Bool joker_available;
+	char name[MAX_NAME_SIZE];
+} Player;
 
-int main(void) {
-	currentState->init();
-	while (1) {	
-		currentState->handle_input();
-		currentState->update();
-		currentState->render();
-	}
-	return 0;
-}
+typedef struct {
+	void (*init) (void);
+	void (*handle_input) (void);
+} State;
+
+State stateMenu = {
+	&State_Menu_init,
+	&State_Menu_handle_input
+};
+State stateCreateUser = {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
+State sstatePlayingtateCreateUser = {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
+State stateCreateUser = {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
+State stateCreateUser = {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
+State statePlaying =  {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
+State stateCreateUser = {
+	&State_CreateUser_init,
+	&State_CreateUser_handle_input
+};
