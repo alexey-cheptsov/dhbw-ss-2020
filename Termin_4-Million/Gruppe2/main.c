a0772f99e522918c91e808aadba2167c16f2c5a0
DHBW-TEL19GR2-et19017 <65611086+DHBW-TEL19GR2-et19017@users.noreply.github.com>
2020-06-13 10:11:27 +0200

    Update main.c

--- Termin_4-Million/Gruppe2/main.c
+++ Termin_4-Million/Gruppe2/main.c
@@ -94,23 +94,23 @@ void print_question_50_50(const Question question)
 {
 	srand(time(0));
 	
-	int num1 = (rand() % (4));					// int num = (rand() % (upper - lower + 1)) + lower;
-	int num2 = (rand() % (4));
+	int num = (rand() % (4));
 	
-	while(num1 == question.nr_correct)
+	while(num == question.nr_correct)
 	{
-		num1 = (rand() % (4));
+		num = (rand() % (4));
 	}
 	
-	while(num2 == question.nr_correct || num2 == num1)
+	if(num < question.nr_correct)
 	{
-		num2 = (rand() % (4));
+		printf("%s \n", question.answers[num]);
+		printf("%s \n", question.answers[question.nr_correct]);
 	}
-	
-	printf("%s \n", question.answers[num1]);
-	printf("%s \n", question.answers[num2]);
-	
-	
+	else
+	{
+		printf("%s \n", question.answers[question.nr_correct]);
+		printf("%s \n", question.answers[num]);
+	}	
 } // Sven
 
 bool check_answer(Question question, int answer)
@@ -121,9 +121,6 @@ bool check_answer(Question question, int answer)
 	}
 	else return false;
 }
-
-void user_input(int *credits, char *str); // Max
-_Bool check_answer(Question question, int answer);
 
 
 // int main(int argc, char ** argv) {
