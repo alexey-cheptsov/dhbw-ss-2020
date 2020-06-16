ff42e44a44aef587ab22ab0fa30184d1744ad551
DHBW-TEL19GR2-et19099 <65611212+DHBW-TEL19GR2-et19099@users.noreply.github.com>
2020-06-16 16:36:49 +0200

    Update anfang.c

--- Termin_4-Million/Gruppe 1/anfang.c
+++ Termin_4-Million/Gruppe 1/anfang.c
@@ -99,10 +99,10 @@ int zufallszahl(int Zahl[8])
 int abfrage50_50(Frage *Catalogue, int index)
 {
 	int a = 0, e=3, r2;
-	static int pruefung = 0;
-	char c, c2, joker;
+	//static int pruefung = 0;
+	char c, c2;
    	double r = e - a + 1;
-	
+	/*
 	if(pruefung == 1)
 	{
 		return 1;
@@ -114,11 +114,10 @@ int abfrage50_50(Frage *Catalogue, int index)
 	
 	if(joker == 'j')
 	{
-	
+	*/
 		r2 = a + (int)(r * rand()/(RAND_MAX+1.0));
 
 		Catalogue = Catalogue + index;		//weiter zur Ausgewählten Frage aus dem Katalog durch Pointeraddition
-							//muss woanders für alle Funtkionen festgelegt werden
 
 		while(r2 == (Catalogue->nr_correct))
 			r2 = a + (int)(r * rand()/(RAND_MAX+1.0));
@@ -139,8 +138,12 @@ int abfrage50_50(Frage *Catalogue, int index)
 			case 3: c2 = 'D';
 		}	
 		printf("Mögliche Antworten sind:\n\n%c %s\n%c %s", c, Catalogue->Antworten[r2], c2, Catalogue->Antworten[Catalogue->nr_correct]);
-		pruefung = 1;
-	} return 0;
+		/*pruefung = 1;
+	} return 0;*/
+	
+	Catalogue = Catalogue - (index -1);
+	
+	return 0;
 }
 
 void fragen_einlesen(Frage* Fragen_Katalog, char* PFAD){
